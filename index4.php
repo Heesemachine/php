f98d429d337b2fd32ad76e6be98eb204bcb4ce3c
Heesemachine <93719680+Heesemachine@users.noreply.github.com>
2022-10-06 22:07:21 +0300

    Update index4.php

--- index4.php
+++ index4.php
@@ -2,11 +2,11 @@
 //Об’єкт “Аукціон” (Код, назва лота, дата початку торгів, дата завершення торгів,
 //стартова ціна, кінцева ціна). Запит торгів за вказану дату із початковою ціною що не перевищує Х.
 session_start();
-$_SESSION["accounting"] = null;
-if (isset($_SESSION["accounting"])){
-    $accounting = $_SESSION["accounting"];
+$_SESSION["aauction"] = null;
+if (isset($_SESSION["aauction"])){
+    $aauction = $_SESSION["aauction"];
 }else{
-    $accounting = [
+    $aauction = [
         [
             "code" => 1,
             "name" => "Glasses",
@@ -26,13 +26,13 @@ if (isset($_SESSION["accounting"])){
         ],
     ];
 }
-function getId($accounting){
-    for($i = 0; $i < count($accounting); $i++){
-        if($_GET["id"] == $accounting[$i]["code"]){
-            $max = $accounting[0]["code"];
-            for($j = 0; $j < count($accounting); $j++){
-                if($accounting[$j]["code"] > $max){
-                    $max = $accounting[$j]["code"];
+function getId($aauction){
+    for($i = 0; $i < count($aauction); $i++){
+        if($_GET["id"] == $aauction[$i]["code"]){
+            $max = $aauction[0]["code"];
+            for($j = 0; $j < count($aauction); $j++){
+                if($aauction[$j]["code"] > $max){
+                    $max = $aauction[$j]["code"];
                 }
             }
             $max++;
@@ -42,15 +42,15 @@ function getId($accounting){
     return $_GET["code"];
 }
 if($_GET["edit"] != null){
-    for($i = 0; $i < count($accounting); $i++){
-        if($_GET["edit"] == $accounting[$i]["code"]){
-            $accounting[$i] = ["code" => getId($accounting),
+    for($i = 0; $i < count($aauction); $i++){
+        if($_GET["edit"] == $aauction[$i]["code"]){
+            $aauction[$i] = ["code" => getId($aauction),
                 "name" => $_GET["name"],
                 "startl" => $_GET["startl"],
                 "endl" => $_GET["endl"],
                 "fprice" => $_GET["fprice"],
                 'lprice' => $_GET['lprice']];
-            $_SESSION["accounting"] = $accounting;
+            $_SESSION["aauction"] = $aauction;
             break;
         }
     }
@@ -76,13 +76,13 @@ else{
         $_GET["lprice"] = "0";
     }
 
-    $accounting[] = ["code" => getId($accounting),
+    $aauction[] = ["code" => getId($aauction),
         "name" => $_GET["name"],
         "startl" => $_GET["startl"],
         "endl" => $_GET["endl"],
         "fprice" => $_GET["fprice"],
         "lprice" => $_GET["lprice"]];
-    $_SESSION["factory"] = $accounting;
+    $_SESSION["factory"] = $aauction;
 }
 function sortBySmth($arr, $fprice,$startl){
     $newArr = [];
@@ -98,9 +98,9 @@ function sortBySmth($arr, $fprice,$startl){
 echo "<h2>Таблиця всіх значень</h2>";
 echo "<table>";
 echo "<tr> <th>Code</th> <th>Name</th> <th>StartLot</th> <th>Endlot</th> <th>FirstPrice</th> <th>Lastprice</th> </tr>";
-for($i = 0; $i < count($accounting); $i++){
+for($i = 0; $i < count($aauction); $i++){
     echo "<tr>";
-    foreach ($accounting[$i] as $key=>$value){
+    foreach ($aauction[$i] as $key=>$value){
         if($value != null){
             echo "<td>$value</td>";
         }
@@ -111,7 +111,7 @@ for($i = 0; $i < count($accounting); $i++){
 }
 echo "</table>";
 
-$arr = sortBySmth($accounting,15000,"16.10.2022");
+$arr = sortBySmth($aauction,15000,"16.10.2022");
 echo "<h2>Таблиця запиту</h2>";
 echo "<table>";
 echo "<tr> <th>Code</th> <th>Name</th> <th>StartLot</th> <th>Endlot</th> <th>FirstPrice</th> <th>Lastprice</th> </tr>";
@@ -146,9 +146,9 @@ echo "</table>";
         <input type="number"  name="code" placeholder="Код"> <br>
         <input type="text"  name="name" placeholder="Імя лота"> <br>
         <input type="text" name="startl" placeholder="Початкова дата"> <br>
-        <input type="number" name="endl"  placeholder="Кінцева Ціна"> <br>
+        <input type="text" name="endl"  placeholder="Кінцева Ціна"> <br>
         <input type="number" name="fprice" placeholder="Початкова ціна"><br>
-        <input type="text" name="lprice" placeholder="Кінцева ціна"><br>
+        <input type="number" name="lprice" placeholder="Кінцева ціна"><br>
         <input type="submit" name="btn-ok" value="ok">
 
 
